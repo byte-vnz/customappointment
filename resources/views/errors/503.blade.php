32d478fc10a84fd2436a69e64b7a72c5efb8cd95
Robinson Cusipag <vnzx01@gmail.com>
2025-10-12 09:09:59 +0800

    downpage

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,37 +1,65 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Parañaque Business Permits and Licensing System</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Parañaque Business Permits and Licensing System</title>
 
-        <!-- Styles -->
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-<title>Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
-<div class="container">
-		<div class="row-md-12">
-			<img src="{{ asset('img/logo/web-city-md.png') }}" alt="tag">
-		</div>
-</div>
+    <!-- Styles -->
 
-<article>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:admin@bploparanaque.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; City Government of Parañaque Business Permits and Licensing Office</p>
+    <title>Site Maintenance</title>
+    <style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+    </style>
+    <div class="container">
+        <div class="row-md-12">
+            <img src="{{ asset('img/logo/web-city-md.png') }}" alt="tag">
+        </div>
     </div>
-</article>
+
+    <article>
+        <h1>We&rsquo;ll be back soon!</h1>
+        <div>
+            <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you
+                can always <a href="mailto:admin@bploparanaque.com">contact us</a>, otherwise we&rsquo;ll be back online
+                shortly!</p>
+            <p>&mdash; City Government of Parañaque</p>
+        </div>
+    </article>
     </body>
+
 </html>
